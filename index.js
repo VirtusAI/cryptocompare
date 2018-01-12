4a1e0c8f9ce80268c2b268a17671d0231de32929
Ricardo <ei10045@fe.up.pt>
2018-01-12 16:51:28 +0000

    retract

--- index.js
+++ index.js
@@ -51,7 +51,6 @@ function exchangeList () {
   const url = `${baseUrl}all/exchanges`
   return fetchJSON(url).then(exchanges => {
     for(let k in exchanges) {
-      exchanges[k].originalKey = k;
       exchanges[k.toUpperCase()] = exchanges[k];
     }
     return exchanges
