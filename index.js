70b4fa3c4db90a9bbeee9b8104d21d2ca8b6f4fa
ricardofmteixeira <ricardofmteixeira@gmail.com>
2018-01-27 15:24:22 +0000

    try to fetch all data

--- index.js
+++ index.js
@@ -162,11 +162,13 @@ function histoHour (fsym, tsym, options) {
   options = options || {}
   if (options.timestamp) options.timestamp = dateToTimestamp(options.timestamp)
   let url = `${baseUrl}histohour?fsym=${fsym}&tsym=${tsym}`
-  if (options.exchange) url += `&e=${options.exchange}`
-  if (options.limit) url += `&limit=${options.limit}`
-  if (options.tryConversion === false) url += '&tryConversion=false'
-  if (options.aggregate) url += `&aggregate=${options.aggregate}`
-  if (options.timestamp) url += `&toTs=${options.timestamp}`
+  if (options.exchange && options.exchange !== 'CCCAGG') url += `&e=${options.exchange}`
+  // if (options.limit) url += `&limit=${options.limit}`
+  // if (options.tryConversion === false) url += '&tryConversion=false'
+
+  url += '&allData=true&limit=2000&tryConversion=false';
+  // if (options.aggregate) url += `&aggregate=${options.aggregate}`
+  // if (options.timestamp) url += `&toTs=${options.timestamp}`
   return fetchJSON(url).then(result => result.Data)
 }
 
